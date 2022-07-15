58a7566f4181938bb6eff419583880696b2ac986
FernandoSilvera <fernando.silvera02@gmail.com>
2022-07-15 16:34:16 +0200

    Class product id and rice added.

--- classes/Product.php
+++ classes/Product.php
@@ -1,7 +1,39 @@
 <?php
 
 class Product {
-  
+  private $price;
+  private $id;
+
+  // CONSTRUCTOR ---------------------------------------------
+  function __construct($_price, $_id) {
+    if (is_nan($_id) || is_nan($_price)) {
+      throw new Exception("You must enter a number");
+    }
+
+    $this->id = $_id;
+    $this->price = $_price;
+  }
+
+  // GETTER AND SETTER ---------------------------------------
+  /**
+   * Get the value of id
+   */ 
+  public function getId()
+  {
+    return $this->id;
+  }
+
+  /**
+   * Set the value of id
+   *
+   * @return  self
+   */ 
+  public function setId($id)
+  { 
+    $this->id = $id;
+
+    return $this;
+  }
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 require_once "classes/Customer.php";
+require_once "classes/Product.php";
 
 $customer1 = new Customer("Fernando", "Silvera");
+$product1 = new Product("Pappa", 999);
 
 var_dump($customer1);
+var_dump($product1);
 ?>
